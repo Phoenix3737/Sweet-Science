481cd1be43e0796e4db58e20852d675d0af97b15
phoenix3737 <groves999@gmail.com>
2018-05-18 13:02:38 -0400

    materialize changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,12 @@
 
 import React from "react";
 import "./Footer.css";
+import {Button} from 'react-materialize'
+
 
 const Footer = () => (
   <footer className="footer">
+  <Button>Cutton</Button>
     <span>Sweet Science</span>
   </footer>
 );
