ac1093f0d808001447b01a8ed68981504cebfee4
phoenix3737 <groves999@gmail.com>
2018-05-18 14:11:58 -0400

    removed 2nd src folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import {Button, Icon} from 'react-materialize'
 import Discover from "./pages/Discover";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
-import Login from "./components/Login";
 
 const App = () => (
   <Router>
     <div>
       <h1>Sweet Science</h1>
       <Navbar />
-        <Login />
-        <Wrapper>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
-        </Wrapper>
+    <Wrapper>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/discover" component={Discover} />
+        <Route exact path="/search" component={Search} />
+      </Wrapper>
       <Footer />
     </div>
   </Router>
 );
 
 export default App;
+export default () => (
+  <Button waves='light'>
+    <Icon>thumb_up</Icon>
+  </Button>
+)
